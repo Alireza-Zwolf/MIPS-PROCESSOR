4631c73e806a6871f14138d13d3e72ba74d7d0cc
Alireza Arbabi <88896798+Alireza-Zwolf@users.noreply.github.com>
2022-07-13 16:34:24 +0430

    Delete MIPS-Single-cycle directory

--- MIPS-Single-cycle/top.sv
+++
@@ -1,7 +0,0 @@
-module MIPS(input clk , rst);
-    wire zero , jlselD , jlselR , jsel , jrsel , ALUsrc , RegDst , RegWrite , MemRead , MemWrite , PCSrc , MemtoReg;
-    wire [2:0] ALUop;
-	wire [5:0] opcode , func;
-    Datapath Datapath(clk , rst , jlselD , jlselR , jsel , jrsel , ALUsrc , RegDst , RegWrite , MemRead , MemWrite , PCSrc , MemtoReg , ALUop , zero , opcode , func);
-    Controller Controller(zero, opcode , func , clk , jlselD , jlselR , jsel , jrsel , ALUsrc , RegDst , RegWrite , MemRead , MemWrite , PCSrc , MemtoReg , ALUop);
-endmodule
