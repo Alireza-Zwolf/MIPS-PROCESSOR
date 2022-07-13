329b321b2720e695076c3e3ff068a80528014b8e
Alireza Arbabi <88896798+Alireza-Zwolf@users.noreply.github.com>
2022-07-13 16:39:22 +0430

    Update TB.sv

--- MIPS-Single-cycle/TB.sv
+++ MIPS-Single-cycle/TB.sv
@@ -1,4 +1,8 @@
 module TB();
+    /*
+    For testing the processor, intsructions.txt Read first 20 numbers of memmory starting from cell 1000,
+    then find the maximum number and store the value and index of it in cell 2000 and 2004.
+    */
     reg clk = 0;
     reg rst = 0;
 
